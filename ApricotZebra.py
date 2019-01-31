ae9b1c7eb34fc2d67551a0fe0c726603d00d1841
ghimiremukesh <ghimiremukesh1@gmail.com>
2019-01-30 18:16:47 -0600

    genetic algorithm to crack password

--- ApricotZebra.py
+++ ApricotZebra.py
@@ -1,167 +1,81 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Sep 13 22:55:47 2018
+Created on Fri Oct 12 13:08:19 2018
 
 @author: cipher
 """
-# Spirit Animal: ApricotZebra
 
-# Date Edited: 09/25/2018 
+# Spirit Name: ApricotZebra
 
-# Challenge #2
+# Challenge 3.5: Genetic Algorithm
 
-#Citations
+# Last Edited: 10/17/2018
 
-# lecutre notes from Dr. Jones 
+# sources consulted: 
+# 1. for random alphanumeric generator:
+    #https://stackoverflow.com/questions/2511222/efficiently-generate-a-16-character-alphanumeric-string/30779367 
+# 2. to read webpage contents:
+    #https://stackoverflow.com/questions/15138614/how-can-i-read-the-contents-of-an-url-with-python
 
-# source for reading file names and count in a directory:
-    # https://stackoverflow.com/questions/2632205/how-to-count-the-number-of-files-in-a-directory-using-python
+#------------Result------------    
+# username=ApricotZebra
+# password=Eteiv8te
 
-# source for getting the path to read files
-    # https://stackoverflow.com/questions/32470543/open-file-in-another-directory-python/32470564
-    
-import pickle    
-import os
-from pathlib import Path
+import random
+import string
 import numpy as np
-import matplotlib.pyplot as plt
-import sys     # to stop the program after invalid entry. 
-
-list =  os.listdir("scripts")
-
-for i in range(0, len(list)):
-     if len(list[i]) == 14:
-         series_1 = open(Path("scripts/"+list[i]),"r").read()
-     else:
-        series_2 = open(Path("scripts/"+list[i]),"r").read()
-
-series_1 = series_1.replace("\n", "")
-
-series_1 = series_1.replace("\t", " ")
-series_1 = series_1.replace("    ", " ")
-series_1 = series_1.replace("   ", " ")
-
-series_1 = series_1.replace("  ", " ")
-
-series_1 = series_1.split(" ")
-del series_1[0]  #it was empty
-
-while '' in series_1:  # too many '' in the list
-    series_1.remove('')
-  
+import requests
 
 
-series_2 = series_2.replace("\n", "")
+username = input("Enter your username for CSCI 343: ")
+parent = 'AAAAAAAA'
+url = 'https://john.cs.olemiss.edu/~jones/csci343/pwd/index.php?username='+username+'&password='+parent
+output = requests.get(url).text
+separate = output.split(' ')
+initial_time = np.float32(separate[0])
+index = 0
+offsprings={}
+index = [0,1,2,3,4,5,6,7]
+   
+def randompwd():
+    randomchar = ''.join([random.choice(string.ascii_letters + string.digits) for n in range(1)])
+    return randomchar
 
-series_2 = series_2.replace("\t", " ")
-series_2 = series_2.replace("    ", " ")
-series_2 = series_2.replace("   ", " ")
+def offspringcheck():
+    global output, parent, index, username, offspring, random_index, offsprings, separate
+    parent = list(parent)
+    random_index = random.choice(index)
+    offspring = parent
+    offspring[random_index] = randompwd()
+    offspring = ''.join(offspring)
+    if not offspring in offsprings:
+        url = 'https://john.cs.olemiss.edu/~jones/csci343/pwd/index.php?username='+username+'&password='+offspring
+        output = requests.get(url).text
+        separate = output.split(' ')
 
-series_2 = series_2.replace("  ", " ")
 
-series_2 = series_2.split(" ")
-
-
-while '' in series_2:
-    series_2.remove('')
-
-
-s1_words = []
-s2_words = []
-
-for i in range(0, len(series_1)):
-        s1_words.append(series_1[i])
-        
-for i in range(0, len(series_2)):
-        s2_words.append(series_2[i])
-        
-
-# Working with the sentiment lexicon file
-
-words=[]
-scores=[]
-file = open('sentiment_lex.csv', 'r')
-lex = file.read()
-lex = lex.split("\n")
-lex.pop() # again empty stuff in list
-lex.pop()
-for index in range(0, len(lex)):
-    lex[index] = lex[index].split(',')
-
-for index in range(0, len(lex)):
-    words.append(lex[index][0])
-    scores.append(lex[index][1])
-
-scores = np.float64(scores)
-
-negative = []
-weakly_negative = []
-neutral = []
-weakly_positive = []
-positive = []
-  
-
-for i in range(0, len(scores)):
-    if scores[i] < -0.6:
-        negative.append(words[i])
-    elif scores[i] >= -0.6 and scores[i] < -0.2:
-        weakly_negative.append(words[i])
-    elif scores[i] >= -0.2 and scores[i] <= 0.2:
-        neutral.append(words[i])
-    elif scores[i] > 0.2 and scores[i] <= 0.6:
-        weakly_positive.append(words[i])
-    else:
-        positive.append(words[i])
-
-# Now comparing words from series to the sentiment lexicon
-        
-name = input('Enter series name to analyze the scripts (A or B): ').upper()
-
-if name == 'A':
-    series = series_1
-elif name == 'B':
-    series = series_2
-else:
-    print('\nInvalid Entry! Enter A or B. Run the program again!\n')
-    sys.exit()
+def main():
+    global separate, parent, initial_time, output, index, offspring, random_index, offsprings
     
-n_count = 0
-wn_count = 0
-neu_count = 0
-wp_count = 0
-p_count = 0
+    offspringcheck()
+    while (output!= 'SUCCESSFUL'):
+        time = np.float32(separate[0])
+        offsprings[offspring] = 0 #just to add it to a dictionary of passwords to not repeat 
+        if time > initial_time:
+            parent = offspring
+            index.remove(random_index)
+            offspringcheck()
+            print(offspring)
+            initial_time = time
+        elif time <= initial_time:
+            print(offspring)
+            offspringcheck()
+       
+    print("\n\nYour password is: " + offspring) 
+        
+main()
 
-for i in range(0, len(series)):
-    for j in range(0, len(negative)):
-        if series[i] == negative[j]:
-            n_count += 1
+
+    
+
             
-    for j in range(0, len(weakly_negative)):
-        if series[i] == weakly_negative[j]:
-           wn_count += 1
-           
-    for j in range(0, len(neutral)):
-        if series[i] == neutral[j]:
-           neu_count += 1
-    for j in range(0, len(weakly_positive)):
-        if series[i] == weakly_positive[j]:
-           wp_count += 1
-    for j in range(0, len(positive)):
-        if series[i] == positive[j]:
-           p_count += 1
-
-# Drawing the bar graph
-bar_pos = [0,1,2,3,4]
-criteria = ['Neg.', 'W.Neg  ', 'Neutral' , 'W. Pos', 'Pos']
-script = np.log10([n_count, wn_count, neu_count, wp_count, p_count])
-plt.bar(bar_pos, script)
-plt.xticks(bar_pos, criteria)
-plt.ylabel('log10(word count)')
-plt.title('Sentiment Analysis for Series ' + name )
-plt.show()
-
-        
-        
-
-      
-
